d27f6cc7398e823cea4d6a21eb922102fc09ce10
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2019-06-24 01:43:22 -0300

    subindo a base da aplicaçao

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect('mongodb+srv://semana:semana@cluster0-a4th6.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser:true});
+
+app.use(require('./routes'));
+app.listen(3000);
